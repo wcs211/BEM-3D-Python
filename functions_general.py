Fixed variable shapes. Fixed sigma calculation. Archive function. Began transform function.

functions_general.py
@@ -84,7 +84,7 @@ def point_vectors(Nc, Ns, v1,v2,v3,v4):
     return (vn_x_unit,vn_y_unit,vn_z_unit)        
 
 
-def archive(array, axis=0):
+def archive(array, axis=3):
     """
     Shifts array values along an axis (row-wise by default).
 
@@ -101,25 +101,35 @@ def archive(array, axis=0):
             array[1:,:] = array[:-1,:]
         else:
             array[:,1:] = array[:,:-1]
+    elif len(np.shape(array)) == 3:
+        if axis == 0:
+            array[1:,:,:] = array[:-1,:,:]
+        elif axis == 1:
+            array[:,1:,:] = array[:,:-1,:]
+        else:
+            array[:,:,1:] = array[:,:,:-1]
+        
 
 # Velocity and velocity potential equations are defined in panel coordinates so a transformation should be done
 # Each row of xp1/xp2/zp is a target, and each column is an influence
 # NI is N influences, NT is N targets
 # xi/zi is x/z of influences, xt/zt is x/z of target points
-def transformation(xt,zt,xi,zi):
+def transformation(xt,yt,zt,xi,yi,zi):
 # Returns xp1, xp2, zp
 # Others: NT, NI, tx, tz, nx, nz, dummy, x_tile, z_tile, tx_tile, tz_tile
 
     NT = np.size(xt)
     NI = np.size(xi)-1
 
-    (tx,tz,nx,nz) = panel_vectors(xi,zi)[:-1]
+    (nx, ny, nz, txs, tys, tzs, txc, tyc, tzc) = panel_vectors(xi,yi,zi)[:-2]
 
     # Intermediary variables to reduce number of tile/repeat operations
     # From normalvectors: tx==nz, tz==-nx
     x_tile = np.repeat(xt[:,np.newaxis],NI,1) - np.repeat(xi[:-1,np.newaxis].T,NT,0)
+    y_tile = np.repeat(yt[:,np.newaxis],NI,1) - np.repeat(yi[:-1,np.newaxis].T,NT,0)
     z_tile = np.repeat(zt[:,np.newaxis],NI,1) - np.repeat(zi[:-1,np.newaxis].T,NT,0)
     tx_tile = np.repeat(tx[:,np.newaxis].T,NT,0)
+    ty_tile = np.repeat(ty[:,np.newaxis].T,NT,0)
     tz_tile = np.repeat(tz[:,np.newaxis].T,NT,0)
 
     # Transforming left side collocation points from global to local coordinates
@@ -141,38 +151,6 @@ def absoluteToBody(Body, Solid, THETA, HEAVE):
 
     Solid.nodesNew[:,0] = (Solid.nodes[:,0] - Body.AF.x_le) * np.cos(-1*THETA) - (Solid.nodes[:,1] - Body.AF.z_le) * np.sin(-1*THETA)
     Solid.nodesNew[:,1] = (Solid.nodes[:,1] - Body.AF.z_le) * np.cos(-1*THETA) + (Solid.nodes[:,0] - Body.AF.x_le) * np.sin(-1*THETA)
-
-def ramp(t, slope, startTime):
-    """
-    This function can generate a ramp signal based on the following inputs:
-
-    Args:
-        t: array of time samples
-        slope: slope of the ramp signal
-        startTime: location where the ramp turns on
-    """
-    # Get the number of samples in the output signal
-    N = t.size
-
-    # Initialize the ramp signal
-    r = np.zeros(N)
-
-    # Find the index where the ramp turns on
-    if (np.median(np.diff(t)) > 0):
-        startInd = np.min((t>=startTime).nonzero())
-        popInd =np.arange(startInd,N)
-    elif (np.median(np.diff(t)) < 0):
-        # Time-reversed ramp
-        startTime = -1. * startTime
-        startInd = np.max((t>=startTime).nonzero())
-        popInd = np.arange(startInd)
-        slope = -1. * slope
-
-    # For indicies greater than the start time, compute the
-    # proper signal value based on slope
-    r[popInd] = slope * (t[popInd] + startTime) - 2 * startTime * slope
-
-    return (r)
 
 def geom_setup(P, PC, Swimmer, solid=None, FSI=None, PyFEA=None):
     SwiP     = [None for x in xrange(P['N_SWIMMERS'])]

functions_influence.py
@@ -101,18 +101,18 @@ def quilt(Swimmers, influence_type, NT, NI, i):
         for SwimI in Swimmers: # Influencing Swimmer (columns)
             if influence_type == 'Body':
                 (c0, cn) = (SwimI.i_b, SwimI.i_b+SwimI.Body.N) # Insertion column range
-                (xi, zi) = (SwimI.Body.AF.x, SwimI.Body.AF.z) # Coordinates of influences
+                (xi, yi, zi) = (SwimI.Body.AF.x, SwimI.Body.AF.z) # Coordinates of influences
             elif influence_type == 'Edge':
                 (c0, cn) = (SwimI.i_e, SwimI.i_e+SwimI.Edge.N)
-                (xi, zi) = (SwimI.Edge.x, SwimI.Edge.z)
+                (xi, yi, zi) = (SwimI.Edge.x, SwimI.Edge.z)
             elif influence_type == 'Wake':
                 (c0, cn) = (SwimI.i_w, SwimI.i_w+i)
-                (xi, zi) = (SwimI.Wake.x[:i+1], SwimI.Wake.z[:i+1])
+                (xi, yi, zi) = (SwimI.Wake.x[:i+1], SwimI.Wake.z[:i+1])
             else:
                 print 'ERROR! Invalid influence type.'
 
             (xp1[r0:rn, c0:cn], xp2[r0:rn, c0:cn], zp[r0:rn, c0:cn]) = \
-                transformation(SwimT.Body.AF.x_col, SwimT.Body.AF.z_col, xi, zi)
+                transformation(SwimT.Body.AF.x_col, SwimT.Body.AF.y_col, SwimT.Body.AF.z_col, xi, yi, zi)
 
     return(xp1, xp2, zp)
 
@@ -200,14 +200,15 @@ def solve_phi(Swimmers, RHO, DEL_T, i, outerCorr):
             for SwimI in Swimmers:
                 a_e[:,SwimI.i_b] = -b_e[:, SwimI.i_e]
                 a_e[:,SwimI.i_b+SwimI.Body.N-1] = b_e[:, SwimI.i_e]
-            a_inv = np.linalg.inv(a_b + a_e)
+            a = a_b + a_e
+
             # Get right-hand side
             if i == 0:
                 b = -np.dot(b_b, sigma_all)
             else:
                 b = -np.dot(b_b, sigma_all) - np.dot(b_w, mu_w_all)
             # Solve for bodies' doublet strengths using explicit Kutta
-            mu_b_all = np.dot(a_inv, b)
+            mu_b_all = np.linalg.solve(a, b)
             # First mu_guess (from explicit Kutta)
             for Swim in Swimmers:
                 Swim.mu_guess = np.empty(2) # [0] is current guess, [1] is previous
@@ -218,7 +219,7 @@ def solve_phi(Swimmers, RHO, DEL_T, i, outerCorr):
         else:
             if n_iter == 2: # Make a second initial guess
                 # Update phi_dinv so it no longer includes explicit Kutta condition
-                a_inv = np.linalg.inv(a_b)
+                a = a_b
 
                 Swimmers[0].mu_guess[1] = Swimmers[0].mu_guess[0]
                 Swimmers[0].delta_cp[1] = Swimmers[0].delta_cp[0]
@@ -237,7 +238,7 @@ def solve_phi(Swimmers, RHO, DEL_T, i, outerCorr):
             else:
                 rhs = -np.dot(b_b, sigma_all) - np.dot(np.insert(b_w, 0, b_e[:,0], axis=1), np.insert(Swimmers[0].Wake.mu[:i], 0, Swimmers[0].mu_guess[0]))
 
-            Swimmers[0].Body.mu = np.dot(a_inv, rhs)
+            Swimmers[0].Body.mu = np.linalg.solve(a, rhs)
 
         for Swim in Swimmers:
             Swim.Body.pressure(RHO, DEL_T, i)

parameter_classes.py
@@ -119,9 +119,6 @@ class BodyBFC(object):
         self.y_mid = y_mid
         self.z_mid = z_mid
         
-#==============================================================================
-# # Make sure about that part.
-#==============================================================================
 class BodyAFC(object):
     """A collection of absolute-frame coordinates for a Body object.
     
@@ -133,20 +130,22 @@ class BodyAFC(object):
         x_neut, y_neut, z_neut: Point arrays along the body's neutral axis.
         x_le, y_le, z_le: Position of the body's leading edge point.
     """
-    def __init__(self, N):
-        self.N = N
-        self.x = np.empty(N+1)
-        self.y = np.empty(N+1)
-        self.z = np.empty(N+1)
-        self.x_col = np.empty(N)
-        self.y_col = np.empty(N)
-        self.z_col = np.empty(N)
-        self.x_mid = np.zeros((3,N))
-        self.y_mid = np.zeros((3,N))
-        self.z_mid = np.zeros((3,N))
-        self.x_neut = np.empty(N)
-        self.y_neut = np.empty(N)
-        self.z_neut = np.empty(N)
+    def __init__(self, N_CHORD, N_SPAN):
+        self.N_CHORD = N_CHORD
+        self.N_SPAN = N_SPAN
+        self.N = 4. * N_CHORD * N_SPAN
+        self.x = np.empty((2 * N_CHORD - 1, N_SPAN))
+        self.y = np.empty((2 * N_CHORD - 1, N_SPAN))
+        self.z = np.empty((2 * N_CHORD - 1, N_SPAN))
+        self.x_col = np.empty((2 * N_CHORD - 2, N_SPAN - 1))
+        self.y_col = np.empty((2 * N_CHORD - 2, N_SPAN - 1))
+        self.z_col = np.empty((2 * N_CHORD - 2, N_SPAN - 1))
+        self.x_mid = np.zeros((2 * N_CHORD - 2, N_SPAN - 1, 3))
+        self.y_mid = np.zeros((2 * N_CHORD - 2, N_SPAN - 1, 3))
+        self.z_mid = np.zeros((2 * N_CHORD - 2, N_SPAN - 1, 3))
+        self.x_neut = np.empty((2 * N_CHORD - 1, N_SPAN))
+        self.y_neut = np.empty((2 * N_CHORD - 1, N_SPAN))
+        self.z_neut = np.empty((2 * N_CHORD - 1, N_SPAN))
         self.x_le = 0.
         self.y_le = 0.
         self.z_le = 0.

swimmer_subclasses.py
@@ -77,24 +77,25 @@ class Body(object):
         self.BF = BodyFrameCoordinates
         # Initialize absolute-frame panel coordinates:
         # x, z, x_col, z_col, x_mid, z_mid, x_neut, z_neut
-        self.AF = PC.BodyAFC(self.N)
+        self.AF = PC.BodyAFC(self.N_CHORD, self.N_SPAN)
         # Prescribed motion
         self.MP = MotionParameters
         self.V0 = MotionParameters.V0
 
-        self.vx = np.zeros(self.N)
-        self.vz = np.zeros(self.N)
+        self.vx = np.zeros((2 * self.N_CHORD - 2, self.N_SPAN - 1))
+        self.vy = np.zeros((2 * self.N_CHORD - 2, self.N_SPAN - 1))
+        self.vz = np.zeros((2 * self.N_CHORD - 2, self.N_SPAN - 1))
 
-        self.sigma = np.zeros(self.N)
-        self.phi_s = np.zeros((self.N,self.N))
-        self.phi_db = np.zeros((self.N,self.N))
-        self.phi_dw = np.zeros(self.N)
-        self.mu = np.zeros(self.N)
-        self.gamma = np.zeros(self.N+1)
+        self.sigma = np.zeros((2 * self.N_CHORD - 2, self.N_SPAN - 1))
+        self.phi_s = np.zeros((2 * self.N_CHORD - 2, self.N_SPAN - 1))
+        self.phi_db = np.zeros((2 * self.N_CHORD - 2, self.N_SPAN - 1))
+        self.phi_dw = np.zeros((2 * self.N_CHORD - 2, self.N_SPAN - 1))
+        self.mu = np.zeros((2 * self.N_CHORD - 2, self.N_SPAN - 1))
+        self.gamma = np.zeros((2 * N_CHORD - 1, N_SPAN))
 
-        self.p = np.zeros(self.N)
-        self.cp = np.zeros(self.N)
-        self.mu_past = np.zeros((2,self.N))
+        self.p = np.zeros((2 * self.N_CHORD - 2, self.N_SPAN - 1))
+        self.cp = np.zeros((2 * self.N_CHORD - 2, self.N_SPAN - 1))
+        self.mu_past = np.zeros((2 * self.N_CHORD - 2, self.N_SPAN - 1, 3))
         
         self.Cf = 0.
         self.Cl = 0.
@@ -145,7 +146,7 @@ class Body(object):
         
         # Stepping through each spanwise position to calculate the positions of
         # the fluke neutral plane at the given time step.
-        for i in range(N_SPAN):   
+        for i in xrange(N_SPAN):   
                    
             A[:,i] = C[i]*((1+EPSILON)**(K-1))*(2**(-K))
             THETA = np.linspace(0,np.pi,N_SPAN)
@@ -170,10 +171,10 @@ class Body(object):
             x[:,i] = np.hstack((x_body[:,i] , x_body[-2::-1,i]))
             z[:,i] = np.hstack((z_bot[:,i] , z_top[-2::-1,i]))
             
-        for i in range(2*N_CHORD-1):
+        for i in xrange(2*N_CHORD-1):
             y[i,:] = np.linspace(0.,SPAN,N_SPAN)
         
-        for i in range(N_SPAN-2):
+        for i in xrange(N_SPAN-2):
             x_mid[:,i] = ((x[1:,i] + x[:-1,i])/2)
             y_mid[:,i] = ((y[1:,i] + y[:-1,i])/2)
             z_mid[:,i] = ((z[1:,i] + z[:-1,i])/2)
@@ -214,7 +215,7 @@ class Body(object):
         oLE = np.zeros(N_SPAN)
         oTE = np.zeros(N_SPAN)
         
-        for i in range(N_SPAN):
+        for i in xrange(N_SPAN):
             # Create bottom x-corrdinates
             start = 0.
             stop  = np.pi
@@ -282,11 +283,11 @@ class Body(object):
             x[:,i] += LE[i]
         
         # Create top y-corrdinates   
-        for i in range(2 * N_CHORD - 1):
+        for i in xrange(2 * N_CHORD - 1):
             y[i,:] = np.linspace(0., SPAN, N_SPAN)
          
         # Define panel mid-points   
-        for i in range(N_SPAN - 2):
+        for i in xrange(N_SPAN - 2):
             x_mid[:,i] = 0.25 * (x[1:,i] + x[:-1,i] + x[1:,i+1] + x[:-1,i+1])
             y_mid[:,i] = 0.25 * (y[1:,i] + y[:-1,i] + y[1:,i+1] + y[:-1,i+1])
             z_mid[:,i] = 0.25 * (z[1:,i] + z[:-1,i] + z[1:,i+1] + z[:-1,i+1])   
@@ -327,7 +328,7 @@ class Body(object):
         
         # Stepping through each spanwise position to calculate the positions of
         # the fluke neutral plane at the given time step.
-        for i in range(N_SPAN):   
+        for i in xrange(N_SPAN):   
                    
             xb[:,i] = np.linspace(np.pi,0.,N_CHORD)
             xt[:,i] = np.linspace(0.,np.pi,N_CHORD)
@@ -364,10 +365,10 @@ class Body(object):
             x[:,i] = np.hstack((xb[:,i] , xt[1:,i]))
             z[:,i] = np.hstack((zb[:,i] , zt[1:,i]))
             
-        for i in range(2*N_CHORD-1):
+        for i in xrange(2*N_CHORD-1):
             y[i,:] = np.linspace(0.,SPAN,N_SPAN)
         
-        for i in range(N_SPAN-2):
+        for i in xrange(N_SPAN-2):
             x_mid[:,i] = ((x[1:,i] + x[:-1,i])/2)
             y_mid[:,i] = ((y[1:,i] + y[:-1,i])/2)
             z_mid[:,i] = ((z[1:,i] + z[:-1,i])/2)
@@ -459,9 +460,9 @@ class Body(object):
         self.AF.x_col = afx_col
         self.AF.y_col = afy_col
         self.AF.z_col = afz_col
-        self.AF.x_mid = x_mid
-        self.AF.y_mid = y_mid
-        self.AF.z_mid = z_mid
+        self.AF.x_mid[:,:,0] = x_mid
+        self.AF.y_mid[:,:,0] = y_mid
+        self.AF.z_mid[:,:,0] = z_mid
         self.AF.x_neut = x_neut
         self.AF.y_neut = y_neut
         self.AF.z_neut = z_neut
@@ -501,9 +502,10 @@ class Body(object):
             THETA_MINUS: Pitching angle minus a small time difference (TSTEP)
             THETA_PLUS: Pitching angle plus a small time difference (TSTEP)
         """
+        Nc = self.BF.x_mid.shape[0]
+        Ns = self.BF.x_mid.shape[1]        
+        
         if i == 0:
-            Nc = self.BF.x.shape[0]-1
-            Ns = self.BF.x.shape[1]-1
             x_col = self.BF.x_mid
             y_col = self.BF.y_mid
             z_col = self.BF.z_mid
@@ -543,19 +545,19 @@ class Body(object):
 
         elif i == 1:
             # First-order backwards differencing of body collocation point positions
-            self.vx = (self.AF.x_mid[0,:]-self.AF.x_mid[1,:])/DEL_T - self.V0
-            self.vy = (self.AF.y_mid[0,:]-self.AF.y_mid[1,:])/DEL_T
-            self.vz = (self.AF.z_mid[0,:]-self.AF.z_mid[1,:])/DEL_T
+            self.vx = (self.AF.x_mid[:,:,0]-self.AF.x_mid[:,:,1])/DEL_T - self.V0
+            self.vy = (self.AF.y_mid[:,:,0]-self.AF.y_mid[:,:,1])/DEL_T
+            self.vz = (self.AF.z_mid[:,:,0]-self.AF.z_mid[:,:,1])/DEL_T
 
         else:
             # Second-order backwards differencing of body collocation point positions
-            self.vx = (3*self.AF.x_mid[0,:]-4*self.AF.x_mid[1,:]+self.AF.x_mid[2,:])/(2*DEL_T) - self.V0
-            self.vy = (3*self.AF.y_mid[0,:]-4*self.AF.y_mid[1,:]+self.AF.y_mid[2,:])/(2*DEL_T)
-            self.vz = (3*self.AF.z_mid[0,:]-4*self.AF.z_mid[1,:]+self.AF.z_mid[2,:])/(2*DEL_T)
+            self.vx = (3*self.AF.x_mid[:,:,0]-4*self.AF.x_mid[:,:,1]+self.AF.x_mid[:,:,2])/(2*DEL_T) - self.V0
+            self.vy = (3*self.AF.y_mid[:,:,0]-4*self.AF.y_mid[:,:,1]+self.AF.y_mid[:,:,2])/(2*DEL_T)
+            self.vz = (3*self.AF.z_mid[:,:,0]-4*self.AF.z_mid[:,:,1]+self.AF.z_mid[:,:,2])/(2*DEL_T)
 
 #        # Body source strengths with normal vector pointing outward (overall sigma pointing outward)
-#        (nx,nz) = panel_vectors(self.AF.x,self.AF.z)[2:4]
-#        self.sigma = nx*(self.V0 + self.vx) + nz*self.vz
+        (nx, ny, nz) = panel_vectors(self.AF.x, self.AF.y, self.AF.z, Ns+1, Nc+1)[0:3]
+        self.sigma = nx*(self.V0 + self.vx) + ny*self.vy + nz*self.vz
 
     def pressure(self, RHO, DEL_T, i):
         """Calculates the pressure distribution along the body's surface.